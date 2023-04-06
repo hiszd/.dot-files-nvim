98821e57406399686d78b29581bc62a9d7e1d206
Zion Koyl <hiszd1@gmail.com>
2023-04-06 11:54:31 -0400

    not sure what I changed

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,6 +1,8 @@
 local init = function()
   require("mini.pairs").setup({})
+
   require("mini.sessions").setup({})
+
   require("mini.surround").setup({
     mappings = {
       add = "<leader>sa",         -- Add surrounding in Normal and Visual modes
@@ -14,12 +16,13 @@ local init = function()
       suffix_next = "",           -- Suffix to search with "next" method
     },
   })
+
   require("mini.starter").setup({
     evaluate_single = true,
     items = {
-      require("mini.starter").sections.builtin_actions(),
-      require("mini.starter").sections.telescope(),
       require("mini.starter").sections.recent_files(5, true),
+      require("mini.starter").sections.telescope(),
+      require("mini.starter").sections.builtin_actions(),
       require("mini.starter").sections.recent_files(2, false),
       -- Use this if you set up 'mini.sessions'
       require("mini.starter").sections.sessions(5, true),
@@ -30,6 +33,7 @@ local init = function()
       require("mini.starter").gen_hook.aligning("center", "center"),
     },
   })
+
   require("mini.indentscope").setup({})
 end
 return {
