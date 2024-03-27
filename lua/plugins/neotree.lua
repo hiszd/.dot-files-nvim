e6ae7be7568c13ba26c56af38b04cd247991da94
Zion Koyl <hiszd1@gmail.com>
2024-03-27 16:43:37 -0400

    Fixed Neotree breaking changes

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -165,7 +165,9 @@ local config = function()
           --".null-ls_*",
         },
       },
-      follow_current_file = true,             -- This will find and focus the file in the active buffer every
+      follow_current_file = {
+        enabled = true,
+      },                                      -- This will find and focus the file in the active buffer every
       -- time the current file is changed while the tree is open.
       group_empty_dirs = false,               -- when true, empty folders will be grouped together
       hijack_netrw_behavior = "open_current", -- netrw disabled, opening a directory opens neo-tree
@@ -202,9 +204,11 @@ local config = function()
       },
     },
     buffers = {
-      follow_current_file = true, -- This will find and focus the file in the active buffer every
+      follow_current_file = {
+        enabled = true,
+      },                       -- This will find and focus the file in the active buffer every
       -- time the current file is changed while the tree is open.
-      group_empty_dirs = true,    -- when true, empty folders will be grouped together
+      group_empty_dirs = true, -- when true, empty folders will be grouped together
       show_unloaded = true,
       window = {
         popup = {
