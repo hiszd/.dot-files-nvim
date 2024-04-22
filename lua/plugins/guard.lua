791a3b446e9e9932e056cc42d36d47bb231123fe
Zion Koyl <hiszd1@gmail.com>
2024-04-22 12:46:44 -0400

    Removed guard because it was buggy kept config in case it works later

--- lua/plugins/guard.lua
+++ lua/plugins/guard.lua
@@ -1,21 +1,24 @@
-local config = function()
-  local ft = require("guard.filetype")
-  local g = require("guard")
-
-  -- multiple files register
-  ft("scss, css"):fmt("prettier")
-
-  require("guard").setup({
-    fmt_on_save = true,
-    lsp_as_default_formatter = true,
-  })
-end
+-- local config = function()
+--   local ft = require("guard.filetype")
+--   local g = require("guard")
+--
+--   -- multiple files register
+--   ft("scss, css"):fmt("prettier")
+--
+--   require("guard").setup({
+--     fmt_on_save = true,
+--     lsp_as_default_formatter = true,
+--   })
+-- end
+--
+-- return {
+--   "nvimdev/guard.nvim",
+--   -- Builtin configuration, optional
+--   dependencies = {
+--     "nvimdev/guard-collection",
+--   },
+--   config = config,
+-- }
 
 return {
-  "nvimdev/guard.nvim",
-  -- Builtin configuration, optional
-  dependencies = {
-    "nvimdev/guard-collection",
-  },
-  config = config,
 }
