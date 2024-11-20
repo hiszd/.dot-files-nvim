d3dab5dc90c8504646f50040f240213f81533080
Zion Koyl <hiszd1@gmail.com>
2024-11-20 12:10:05 -0500

    add breadcrumb to oil.nvim

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -3,13 +3,29 @@ local init = function()
   map({ "n" }, "<leader>n", function() require("oil").open() end, { noremap = true })
 end
 
+-- Declare a global function to retrieve the current directory
+function _G.get_oil_winbar()
+  local dir = require("oil").get_current_dir()
+  if dir then
+    return vim.fn.fnamemodify(dir, ":~")
+  else
+    -- If there is no current directory (e.g. over ssh), just show the buffer name
+    return vim.api.nvim_buf_get_name(0)
+  end
+end
+
 return {
   {
     'stevearc/oil.nvim',
     opts = {
+      default_file_explorer = true,
+      view_options = {
+        show_hidden = true,
+      },
       win_options = {
+        winbar = "%!v:lua.get_oil_winbar()",
         wrap = false,
-        signcolumn = "no",
+        signcolumn = "yes",
         cursorcolumn = false,
         foldcolumn = "0",
         spell = false,
